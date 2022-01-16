c435bb290e134e5c6a8477a2902d14d4025cd589
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-01-16 20:14:05 +0530

    Book Routing

--- book-store-api/index.js
+++ book-store-api/index.js
@@ -2,12 +2,17 @@
 
 // Step 1: Import Express package
 const express = require("express");
+const bookRoutes = require("./routers/book");
 
 // Step 2: Create Server
 const server = express();
 
 // Step 3: Configure port
 server.listen(3100);
+
+// Custom Routes
+// Redirect book related requests to book routes.
+server.use('/api/Book', bookRoutes);
 
 // Step 4: Handle request on default path
 server.get("/", (req, res)=>{
---
+++ book-store-api/routers/book.js
@@ -0,0 +1,11 @@
+
+// Step 1: Import Express
+const express = require("express");
+
+// Step 2: Create Router
+const router = express.Router();
+
+// Step 3: Configure Paths
+router.get('/', controllerCode);
+
+module.exports = router;
